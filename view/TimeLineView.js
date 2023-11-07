1a3bf35a5222f1e8851ae0dfd6cbd50d4f052924
danielgugger <71376563+danielgugger@users.noreply.github.com>
2023-11-07 13:20:56 +0100

    added svg

---
+++ view/TimeLineView.js
@@ -0,0 +1,27 @@
+const TimeLineView = (timeInputController, lineElement) => {
+    const render = () => {
+
+        // Specify the path to your SVG file
+        const svgFilePath = "./../static/assets/timeLine.svg";
+
+        // Use the fetch API to retrieve the SVG file
+        fetch(svgFilePath)
+            .then((response) => response.text())
+            .then((svgContent) => {
+                // Create an SVG element
+                const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+                svg.setAttribute("width", "400");
+                svg.setAttribute("height", "300");
+
+                // Append the SVG content from the file to the SVG element
+                svg.innerHTML = svgContent;
+
+                // Append the SVG element to the container
+                lineElement.appendChild(svg);
+            })
+            .catch((error) => {
+                console.error("Error loading SVG file:", error);
+            });
+    }
+    render();
+}
