73a1b5e6b5378b08d36d194ae472673f7a5a2186
danielgugger <71376563+danielgugger@users.noreply.github.com>
2023-11-14 13:01:16 +0100

    added bar to timeline

--- view/TimeLineView.js
+++ view/TimeLineView.js
@@ -1,3 +1,4 @@
+/*
 const TimeLineView = (timeInputController, lineElement) => {
     const render = () => {
 
@@ -8,18 +9,64 @@ const TimeLineView = (timeInputController, lineElement) => {
         fetch(svgFilePath)
             .then((response) => response.text())
             .then((svgContent) => {
-                // Create an SVG element
-                const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+                // Create a div element to act as a container
+                const container = document.createElement("div");
 
-                // Append the SVG content from the file to the SVG element
-                svg.innerHTML = svgContent;
+                // Append the SVG content from the file to the container
+                container.innerHTML = svgContent;
 
-                // Append the SVG element to the container
-                lineElement.appendChild(svg);
+                // Append the container to the main lineElement
+                lineElement.appendChild(container);
             })
             .catch((error) => {
                 console.error("Error loading SVG file:", error);
             });
+    }
+    render();
+}*/
+
+
+const TimeLineView = (timeInputController, lineElement) => {
+    const render = () => {
+        const timelineSvgFilePath = "./../static/assets/timeLine.svg";
+        const barSvgFilePath = "./../static/assets/Group 83.svg"; // Path to your second SVG
+
+        // Fetch and load the timeline SVG
+        fetch(timelineSvgFilePath)
+            .then(response => response.text())
+            .then(svgContent => {
+                // Create a container for the SVGs
+                const svgContainer = document.createElement("div");
+                svgContainer.style.position = "relative"; // Set position to relative for inner positioning
+
+                // Append the timeline SVG to the container
+                const timelineContainer = document.createElement("div");
+                timelineContainer.innerHTML = svgContent;
+                svgContainer.appendChild(timelineContainer);
+
+                // Fetch and load the bar SVG
+                fetch(barSvgFilePath)
+                    .then(response => response.text())
+                    .then(svgContent => {
+                        // Create a container for the bar SVG
+                        const barContainer = document.createElement("div");
+                        barContainer.innerHTML = svgContent;
+
+                        // Style the barContainer for overlay
+                        barContainer.style.position = "absolute";
+                        barContainer.style.top = '31%'; // Adjust these values as needed
+                        barContainer.style.left = '2.45%'; // Adjust these values as needed
+                        barContainer.style.width = "100%";
+
+                        // Append the bar SVG to the container
+                        svgContainer.appendChild(barContainer);
+
+                        // Append the SVG container to the main lineElement
+                        lineElement.appendChild(svgContainer);
+                    })
+                    .catch(error => console.error("Error loading bar SVG file:", error));
+            })
+            .catch(error => console.error("Error loading timeline SVG file:", error));
     }
     render();
 }
